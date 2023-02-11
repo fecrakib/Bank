0a41dac21397e0961371c1b0c4f5e1747704e75c
fecrakib <rakibislam@132575gmial.com>
2023-02-11 22:59:02 +0600

    font-pages complite

---
+++ index.js
@@ -0,0 +1,7 @@
+
+document.getElementById('btn-submit').addEventListener('click',function(){
+    const emial=document.getElementById('email').value
+const password=document.getElementById('password').value
+console.log(password,emial);
+
+})
