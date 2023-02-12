692aa20bbbc5ecf7d89f94dd9c1855e77625359b
fecrakib <rakibislam@132575gmial.com>
2023-02-12 10:50:40 +0600

    secount-page conmplite

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-
+// const emialFild=document.getElementById('email')
+// const email=emialFild.value
+// const passwordFild=document.getElementById('password')
+// const password=passwordFild.value
+// if(email==='rakibislam@132575gamil.com' && password==='rakib'){
+//     console.log("usser valid")
+// }
+// else{
+//     console.log("unvalid");
+   
+// }
 document.getElementById('btn-submit').addEventListener('click',function(){
-    const emial=document.getElementById('email').value
-const password=document.getElementById('password').value
-console.log(password,emial);
+
+    const emialFild=document.getElementById('email')
+    const email=emialFild.value
+    const passwordFild=document.getElementById('password')
+    const password=passwordFild.value
+    if(email==='rakibislam@132575gamil.com' && password==='rakib'){
+        console.log("usser valid")
+      window.location.href='bank.html'
+       
+    }
+    else{
+        console.log("unvalid");
+       
+    }
+    // validation();
 
 })
+// email validation
+function validation(){
+    if(email==='rakibislam@132575gamil.com' && password==='rakib'){
+        console.log("usser valid")
+    }
+    else{
+        console.log("unvalid");
+       
+    }
+}
